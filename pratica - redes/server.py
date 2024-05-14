b2cf7ecd172b0653644455d21c32bf0fe77265e9
phleao <pedroleao_@hotmail.com>
2024-05-14 20:29:33 -0300

    codigo comentado

--- pratica - redes/server.py
+++ pratica - redes/server.py
@@ -1,3 +1,4 @@
+# Importação das bibliotecas necessárias
 from Crypto.Protocol.KDF import scrypt
 from pyotp.totp import TOTP
 import qrcode
@@ -6,100 +7,149 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA256
 from Crypto.Cipher import AES
 
+# Definição da classe Server
 class Server:
+    # Método de inicialização da classe
     def __init__(self):
-        #dicionario usuario - chave
+        # Dicionário para armazenar os usuários e suas chaves
         self.__users = {}
+        # Chave de sessão, inicializada como None
         self.__session_key = None
 
+    # Getter para a chave de sessão
     @property
     def session_key(self):
         return self.__session_key
 
+    # Setter para a chave de sessão
     @session_key.setter
     def session_key(self, key):
         self.__session_key = key
 
+    # Getter para os usuários
     @property
     def users(self):
         return self.__users
 
+    # Método para aplicar o algoritmo scrypt
     def apply_scrypt(self, namekey, salt):
+        # Embaralha o salt utilizando uma cifra de César simples
         salt = self.ceaser_cypher(salt, len(salt))
+        # Aplica o algoritmo scrypt para criar uma chave
+            # namekey = user + senha
+            # salt = cifra de cesar no username usando tamanho como chave
+            # 32 =  comprimento da chave de saida (bytes)
+            # N = custo de memoria
+            # r = tamanho sequencia
+            # p = paralelismo
         scrypt_key = scrypt(namekey, salt, 32, N=16384, r=8, p=1)
         return scrypt_key
 
+    # Método para registrar um novo usuário no sistema
     def user_register(self, name, key):
+        # Verifica se o usuário já está cadastrado
         if name in self.users.keys():
-            print('\n----- Usuário ja cadastrado no sistema!! -----')
+            print('\n----- Usuário já cadastrado no sistema!! -----')
             return False
 
+        # Aplica o algoritmo scrypt para gerar a chave do usuário
         scrypt_key = self.apply_scrypt(name.encode()+key, name)
+        # Armazena o usuário e sua chave no dicionário de usuários
         self.users[name] = scrypt_key
+        # Gera um código TOTP para o usuário ()
         totp = TOTP(base64.b32encode(scrypt_key)).provisioning_uri(name=name, issuer_name='UFSCJoao-Pedro')
+        # Gera um QR code com o código TOTP
         qrcode_image = qrcode.make(totp)
 
         return qrcode_image
 
+    # Método para verificar se um usuário existe e a chave fornecida corresponde à chave registrada
     def seek_user(self, name, key):
+        # Aplica o algoritmo scrypt para gerar a chave do usuário
         scrypt_key = self.apply_scrypt(name.encode()+key, name)
 
+        # Verifica se o usuário está no dicionário de usuários e se a chave fornecida corresponde à chave registrada
         if name in self.users.keys():
             if self.users[name] == scrypt_key:
                 return True
         return False
 
+    # Método para criar uma chave a partir de uma senha e um salt utilizando o algoritmo PBKDF2
     def create_key(self, pwd, salt):
+        # Embaralha o salt utilizando uma cifra de César simples
         salt = self.ceaser_cypher(salt, len(salt))
+        # Aplica o algoritmo PBKDF2 para criar a chave
         key = PBKDF2(pwd, salt, count=1000, hmac_hash_module=SHA256)
         return key
 
+    # Método para aplicar o segundo fator de autenticação utilizando um código TOTP
     def apply_2factor(self, name, key, totp_input):
+        # Aplica o algoritmo scrypt para gerar a chave do usuário
         scrypt_key = self.apply_scrypt(name.encode()+key, name)
+        # Cria um objeto TOTP com a chave do usuário
         totp = TOTP(base64.b32encode(scrypt_key))
+        # Verifica se o código TOTP fornecido pelo usuário está correto
         if totp.verify(totp_input):
+            # Cria uma chave de sessão
             self.session_key = self.create_key(totp_input, name)
             return True
         return False
 
+    # Método para criptografar uma mensagem utilizando AES-GCM
     def autenticated_encrypt(self, msg, session_key):
+        # Converte a mensagem para bytes
         msg = msg.encode()
 
+        # Cria um objeto AES com o modo de operação GCM
         cipher = AES.new(session_key, AES.MODE_GCM)
+        # Criptografa a mensagem e calcula o código de autenticação (MAC)
         encrypted_msg, mac = cipher.encrypt_and_digest(msg)
 
         return cipher.nonce + mac + encrypted_msg
 
+    # Método para descriptografar uma mensagem e verificar sua autenticidade
     def autenticated_decrypt(self, concatenated_msg, session_key):
+        # Divide a mensagem concatenada em nonce, MAC e mensagem criptografada
         nonce = concatenated_msg[:16]
         mac = concatenated_msg[16:32]
         encrypted_msg = concatenated_msg[32:]
 
+        # Cria um objeto AES com o modo de operação GCM e o nonce fornecido
         cipher = AES.new(session_key, AES.MODE_GCM, nonce=nonce)
         try:
+            # Descriptografa a mensagem e verifica o MAC
             decrypted_msg = cipher.decrypt_and_verify(encrypted_msg, mac)
         except ValueError:
+            # Em caso de erro, a mensagem foi modificada
             print("Cuidado, a mensagem foi modificada!!")
             return False
 
+        # Converte a mensagem descriptografada para string, se necessário
         if isinstance(decrypted_msg, bytes):
             decrypted_msg = decrypted_msg.decode()
 
         return decrypted_msg
 
-
+    # Método para receber uma mensagem criptografada, descriptografá-la e enviar uma resposta criptografada
     def receive_than_send_msg(self, msg):
+        # Descriptografa a mensagem recebida
         decrypted_msg = self.autenticated_decrypt(msg, self.session_key)
         if decrypted_msg:
+            # Cria uma resposta à mensagem recebida
             msg_to_send = f"Lido mensagem '{decrypted_msg}', mas usuário, eu sou uma máquina sem IA, por enquanto ainda não consigo gerar respostas legais..."
+            # Criptografa a resposta e a envia
             return self.autenticated_encrypt(msg_to_send, self.session_key)
         return False
 
+    # Método para aplicar a cifra de César
     def ceaser_cypher(self, text, key):
         encrypted = ""
         for char in text:
+            # Verifica se o caractere é uma letra
             if char.isalpha():
+                # Aplica a cifra de César para embaralhar o caractere
                 encrypted += chr((ord(char) + key - 97) % 26 + 97)
             else:
+                # Mantém caracteres não alfabéticos inalterados
                 encrypted += char
         return encrypted
--- pratica - redes/user.py
+++ pratica - redes/user.py
@@ -4,71 +4,90 @@ from Crypto.Cipher import AES
 
 class User:
     def __init__(self, server):
+        # Inicialização do objeto User
         self.server = server
         self.__session_key = None
 
+    # Getter para a chave de sessão
     @property
     def session_key(self):
         return self.__session_key
 
+    # Setter para a chave de sessão
     @session_key.setter
     def session_key(self, key):
         self.__session_key = key
 
+    # Método para criar uma chave a partir de uma senha e um salt utilizando o algoritmo PBKDF2
     def create_key(self, pwd, salt):
         salt = self.ceaser_cypher(salt, len(salt))
         key = PBKDF2(pwd, salt, count=1000, hmac_hash_module=SHA256)
         return key
 
+    # Método para criar um novo usuário
     def create_user(self):
+        # Solicita o nome de usuário e a senha ao usuário
         name = input('Nome de usuário: ')
         pwd = input('Senha: ')
+        
+        # Verifica se o nome de usuário e a senha não estão vazios
         if not name or not pwd:
             print('\n----- Senha e Nome de usuário não podem estar vazios!! -----')
             return
+        
+        # Cria uma chave a partir da senha e do nome de usuário
         key = self.create_key(pwd, name)
+        
+        # Chama o método de registro de usuário do servidor, passando o nome de usuário e a chave
         qrcode_image = self.server.user_register(name, key)
         qrcode_image.show()
 
         print('\n----- Usuário cadastrado!! -----')
 
+    # Método para realizar o login do usuário
     def login(self):
+        # Solicita o nome de usuário e a senha ao usuário
         name = input('Nome de usuário: ')
         pwd = input('Senha: ')
 
+        # Cria uma chave a partir da senha e do nome de usuário
         key = self.create_key(pwd, name)
 
+        # Verifica se o usuário e a senha são válidos
         if not self.server.seek_user(name, key):
             print('\n----- Usuário e/ou senha inválidos!! -----')
             return False
 
+        # Tentativas para o segundo fator de autenticação
         trys = 0
         while trys <= 2:
             totp_input = input('\nDigite o código de autenticação de 2 fatores do App: ')
-            if self.server.apply_2factor(name,key, totp_input):
+            if self.server.apply_2factor(name, key, totp_input):
                 print('\n------ Login realizado com sucesso  ------'
                       '\n--------------- Bem Vindo ---------------')
+                # Cria uma chave de sessão
                 self.session_key = self.create_key(totp_input, name)
                 return True
             trys += 1
-            print(f"\nCódigo de autenticação de 2 fatores invalido! Chances: {trys}/3")
+            print(f"\nCódigo de autenticação de 2 fatores inválido! Tentativas restantes: {trys}/3")
 
         print('\n-- Tentativas excedidas do segundo fator --')
         return False
 
+    # Método para criptografar uma mensagem utilizando AES-GCM
     def autenticated_encrypt(self, msg, session_key):
         msg = msg.encode()
 
         cipher = AES.new(session_key, AES.MODE_GCM)
-        encrypted_msg, mac = cipher.encrypt_and_digest(msg) # hmac com SHA25
+        encrypted_msg, mac = cipher.encrypt_and_digest(msg)
 
         return cipher.nonce + mac + encrypted_msg
 
+    # Método para descriptografar uma mensagem e verificar sua autenticidade
     def autenticated_decrypt(self, concatenated_msg, session_key):
         nonce = concatenated_msg[:16]
         mac = concatenated_msg[16:32]
         encrypted_msg = concatenated_msg[32:]
-
 
         cipher = AES.new(session_key, AES.MODE_GCM, nonce=nonce)
         try:
@@ -82,13 +101,18 @@ class User:
 
         return decrypted_msg
 
+    # Método para enviar uma mensagem criptografada e receber uma resposta criptografada do servidor
     def send_than_receive_msg(self, msg):
+        # Criptografa a mensagem utilizando a chave de sessão
         encrypted_msg = self.autenticated_encrypt(msg, self.session_key)
+        # Envia a mensagem criptografada para o servidor e recebe uma resposta criptografada
         response_msg = self.server.receive_than_send_msg(encrypted_msg)
         if response_msg:
+            # Descriptografa a resposta recebida do servidor
             decrypted_msg = self.autenticated_decrypt(response_msg, self.session_key)
             print(decrypted_msg)
 
+    # Método para aplicar a cifra de César
     def ceaser_cypher(self, text, key):
         encrypted = ""
         for char in text:
